8876f26e4ca85aae08ee02f8bd8931a9fcb49848
jasonxanthakis <iasonas.xanthakis@gmail.com>
2024-01-30 16:37:59 +0000

    Emergency commit: changed run-script.sh

--- Standardised_MD_Script/run-script.sh
+++ Standardised_MD_Script/run-script.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-current_directory=$(pwd)
-cd ~
-source /home/ixanthakis/wsl_repos/Dissertation_Repos/nsimenv3/bin/activate
-cd $current_directory
+#current_directory=$(pwd)
+#cd ~
+#source /home/ixanthakis/wsl_repos/Dissertation_Repos/nsimenv3/bin/activate
+#cd $current_directory
 
-#python MD-script.py -model Hay2011L5bPyramidalCell -filepath HayModel/ -netfile HayModel/TestL5PC.net.nml \
-#                    -cellfile HayModel/L5PC.cell.nml -lemsfile HayModel/LEMS_TestL5PC.xml \
-#                    -start "-0.5" -end "1.0" -step "0.05" -list "(-1.0, -0.5, 1.0)"
+python MD-script.py -model Hay2011L5bPyramidalCell -filepath HayModel/ -netfile HayModel/TestL5PC.net.nml \
+                    -cellfile HayModel/L5PC.cell.nml -lemsfile HayModel/LEMS_TestL5PC.xml \
+                    -start "-0.5" -end "1.0" -step "0.05" -list "(-1.0, -0.5, 1.0)"
 
-#python MD-script.py -model Hay2011L5bPyrSoma -filepath HayModel/ -netfile HayModel/L5bPyrCellHayEtAl2011.net.nml \
-#                    -cellfile HayModel/Soma_AllCML.cell.nml -lemsfile HayModel/LEMS_L5bPyrCellHayEtAl2011.xml \
-#                    -start "-0.04" -end "0.1" -step "0.005" -list "(-0.01,-0.005,0.01,0.05)"
+python MD-script.py -model Hay2011L5bPyrSoma -filepath HayModel/ -netfile HayModel/L5bPyrCellHayEtAl2011.net.nml \
+                    -cellfile HayModel/Soma_AllCML.cell.nml -lemsfile HayModel/LEMS_L5bPyrCellHayEtAl2011.xml \
+                    -start "-0.04" -end "0.1" -step "0.005" -list "(-0.01,-0.005,0.01,0.05)"
 
-#python MD-script.py -model Bahl2012ReducedL5PyramidalCell -filepath BahlModel/ -netfile BahlModel/pyr_multi_comp_original.net.nml \
-#                    -cellfile BahlModel/pyr.cell.nml -lemsfile BahlModel/LEMS_pyr_multi_comp_original.xml \
-#                    -start "-0.5" -end "1.0" -step "0.05" -list "(-1.0, -0.5, 1.0)"
+python MD-script.py -model Bahl2012ReducedL5PyramidalCell -filepath BahlModel/ -netfile BahlModel/pyr_multi_comp_original.net.nml \
+                    -cellfile BahlModel/pyr.cell.nml -lemsfile BahlModel/LEMS_pyr_multi_comp_original.xml \
+                    -start "-0.5" -end "1.0" -step "0.05" -list "(-1.0, -0.5, 1.0)"
 
 python MD-script.py -model Yao2022HumanL23Pyr -filepath Yao_2022_Model/NeuroML2/ \
                     -netfile Yao_2022_Model/NeuroML2/HL23PYR.net.nml \
